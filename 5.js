1c24298244091f7a0990e2bd57038c64534245fc
Sasha <sasha.8556@mail.ru>
2022-09-01 17:44:49 +0300

    5

--- 5.js
+++ 5.js
@@ -13,9 +13,20 @@ console.log(ucFirst('sasha'));
 // Напишите функцию checkSpam(str), возвращающую true, если str содержит 'badWord' или 'XXX', а иначе false.
 // Функция должна быть нечувствительна к регистру.
 
-const checkSpam = (str) => {
-    let lowerStr = str.lowerCase();
-    return lowerStr.includes('badWord') || lowerStr.includes('XXX')
+// const checkSpam = (str) => {
+//     let lowerStr = str.lowerCase();
+//     return lowerStr.includes('badWord') || lowerStr.includes('XXX')
 
+// }
+// console.log(.includes('badWord'));
+
+// Задание 3
+// Написать функцию, которой на вход подается строка, на выход она дает символы наоборот (разворачивает строку). Пример: «привет Женя» -> «янеЖ тевирп»
+// Обратите внимание: метод reverse существует только у массивов.
+
+
+const arr = (string) => {
+
+    return string.split("").reverse().join("");
 }
-console.log(srt.includes('badWord'));
+console.log(arr('привет Женя'));
